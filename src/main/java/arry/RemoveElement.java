8052c34cfd90ea7ed113b42a8c0dd6421eb121fa
zhangxianglong006 <zhangxianglong006@ke.com>
2022-06-02 07:30:30 +0800

    数据移除元素

--- src/main/java/arry/RemoveElement.java
+++ src/main/java/arry/RemoveElement.java
@@ -1,6 +1,9 @@
 package arry;
 
 /**
+ * 27. 移除元素（还没理解）
+ * https://leetcode.cn/problems/remove-element/
+ *
  * @description：删除数组中指定的元素
  * @author: zhangxianglong
  * @date: 2022/5/14
@@ -16,8 +19,30 @@ public class RemoveElement {
         return idx;
     }
 
+    /**
+     * 自己的理解方法
+     *
+     * @param nums
+     * @param val
+     * @return
+     */
+    static int removeElement2(int[] nums, int val) {
+        int fast = 0;
+        int slow = 0;
+        while (fast < nums.length) {
+            if (nums[fast] != val) {
+                nums[slow] = nums[fast];
+                slow++;
+                fast++;
+            } else {
+                fast++;
+            }
+        }
+        return slow;
+    }
+
     public static void main(String[] args) {
         int[] nums = new int[]{3, 2, 2, 3};
-        int length = removeElement(nums, 3);
+        int length = removeElement2(nums, 3);
     }
 }

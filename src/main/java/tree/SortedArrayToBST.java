b24c78a1e3ed2b6ea7a21d534ae0a7bc925a608d
zhangxianglong006 <zhangxianglong006@ke.com>
2022-06-09 12:04:30 +0800

    有序数组转为二叉树

--- src/main/java/tree/SortedArrayToBST.java
+++ src/main/java/tree/SortedArrayToBST.java
@@ -8,4 +8,32 @@ package tree;
  * @date: 2022/6/8
  */
 public class SortedArrayToBST {
+
+    public static TreeNode sortedArrayToBST(int[] nums) {
+        TreeNode treeNode = builder(nums, 0, nums.length - 1);
+
+        return treeNode;
+
+    }
+
+
+    public static TreeNode builder(int[] nums, int left, int right) {
+        if (left > right) {
+            return null;
+        }
+
+        int middle = (right + left) / 2;
+
+        TreeNode root = new TreeNode(nums[middle]);
+
+        root.left = builder(nums, left, middle - 1);
+        root.right = builder(nums, middle + 1, right);
+
+        return root;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = new int[]{-10, -3, 0, 5, 9};
+        TreeNode root = sortedArrayToBST(nums);
+    }
 }

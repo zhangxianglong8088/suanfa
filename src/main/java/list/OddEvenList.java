e42973fc5a205fbd7aca722d3582a32a43b40bd4
zhangxianglong006 <zhangxianglong006@ke.com>
2022-06-12 20:14:00 +0800

    链表相关题目优化

---
+++ src/main/java/list/OddEvenList.java
@@ -0,0 +1,13 @@
+package list;
+
+/**
+ * @description：https://leetcode.cn/problems/odd-even-linked-list/ 328. 奇偶链表
+ * @author: zhangxianglong
+ * @date: 2022/6/12
+ */
+public class OddEvenList {
+
+    public ListNode oddEvenList(ListNode head) {
+        return null;
+    }
+}
---
+++ src/main/java/list/SsortedListToBST.java
@@ -0,0 +1,19 @@
+package list;
+
+import tree.TreeNode;
+
+/**
+ * 109. 有序链表转换二叉搜索树
+ * https://leetcode.cn/problems/convert-sorted-list-to-binary-search-tree/
+ *
+ * @description：
+ * @author: zhangxianglong
+ * @date: 2022/6/12
+ */
+public class SsortedListToBST {
+
+    public TreeNode sortedListToBST(ListNode head) {
+        return null;
+
+    }
+}

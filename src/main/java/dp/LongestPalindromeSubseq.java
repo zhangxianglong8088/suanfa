079ec7ccdf78d3cf4b6cd854bd9e6fa8894c942c
zhangxianglong006 <zhangxianglong006@ke.com>
2022-06-25 09:43:05 +0800

    求最大回文子序列长度

--- src/main/java/dp/LongestPalindromeSubseq.java
+++ src/main/java/dp/LongestPalindromeSubseq.java
@@ -10,13 +10,34 @@ package dp;
  */
 public class LongestPalindromeSubseq {
 
-    public int longestPalindromeSubseq(String s) {
+    public static int longestPalindromeSubseq(String s) {
+        int len = s.length();
+        //定义dp数组 dp[i][j]  i和j区间内的最大回文子序列长度
+        int[][] dp = new int[len + 1][len + 1];
 
-        return 0;
+        //遍历dp数组
+        for (int i = len - 1; i >= 0; i--) {
+            //当i和j相同的时候 长度为1
+            dp[i][i] = 1;
+            for (int j = i + 1; j < len; j++) {
 
+                if (s.charAt(i) == s.charAt(j)) {
+                    //如果 i和j的值相同
+                    dp[i][j] = dp[i + 1][j - 1] + 2;
+                } else {
+                    //如果 i和j的值不相同 选择将i加入或者将j加入 取最大值
+                    dp[i][j] = Math.max(dp[i + 1][j], dp[i][j - 1]);
+                }
+            }
+        }
+
+        return dp[0][len - 1];
     }
 
     public static void main(String[] args) {
 
+        int len = longestPalindromeSubseq("bbbab");
+
+        System.out.println(len);
     }
 }
